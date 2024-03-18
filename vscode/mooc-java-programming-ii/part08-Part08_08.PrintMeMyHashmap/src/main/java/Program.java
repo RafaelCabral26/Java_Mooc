e867ba3a801d309d2de2d332817fe31a6982cd25
RafaelCabral26 <rafaelcabral261992@gmail.com>
2024-03-18 18:05:01 -0300

    8-pt2 finished

--- vscode/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ vscode/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -1,10 +1,31 @@
-
 import java.util.HashMap;
 
 public class Program {
 
     public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
         // Test your program here!
     }
+
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String name : hashmap.keySet()) {
+            System.out.println(name);
+        }
+    }
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String name : hashmap.keySet()) {
+            if (name.contains(text)) {
+                System.out.println(name);
+            }
+        }
+    }
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String value : hashmap.keySet()) {
+            if (value.contains(text)) {
+                System.out.println(hashmap.get(value));
+            }
+        }
+    }
+
 
 }
